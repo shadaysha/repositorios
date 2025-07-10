b59a55f4880900d3610f77c4589eb5e134f2ba99
shadaysha <shadayshaarias@gmail.com>
2025-07-10 10:01:26 -0600

    Ya esta

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
 
-dotenv.config();//Llamar el archivo .env
-mongoose.connect(process.env.urlbd)//Conectar con mongodb
+dotenv.config();//Llamar el archivo .env.
+mongoose.connect(process.env.urlbd)//Conectar con mongodb.
    .then(() =>{
     console.log("todo jalo chingon con la base de datos")
     })
